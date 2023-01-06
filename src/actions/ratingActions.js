fd63f878ad847000f542ef99bc0b4da1a67d999f
Twoje Imię Nazwisko <toplelfaget@gmail.com>
2023-01-06 23:57:19 +0100

    that one /

--- src/actions/ratingActions.js
+++ src/actions/ratingActions.js
@@ -1,51 +1,48 @@
 import {
-    NOTES_RATE_FAIL,
-    NOTES_RATE_REQUEST,
-    NOTES_RATE_SUCCESS,
-  } from "../constants/noteConstants";
-  import axios from "axios";
-  import uri from '../uri';
-  
-  export const rateNote = (noteId, rating) => async (dispatch, getState) => {
-    try {
-      dispatch({
-        type: NOTES_RATE_REQUEST,
-      });
-  
-      const {
-        userLogin: { userInfo },
-      } = getState();
-  
-      const config = {
-        headers: {
-          Authorization: `Bearer ${userInfo.token}`,
-        },
-      };
-  
-      const { data } = await axios.put(uri+
-        `api/rating/${noteId}/rating`,
-        {
-          userId: userInfo._id,
-          rating,
-        },
-        config
-      );
-  
-      dispatch({
-        type: NOTES_RATE_SUCCESS,
-        payload: data,
-      });
-    } catch (error) {
-      const message =
-        error.response && error.response.data.message
-          ? error.response.data.message
-          : error.message;
-      dispatch({
-        type: NOTES_RATE_FAIL,
-        payload: message,
-      });
-    }
-  };
-  
+  NOTES_RATE_FAIL,
+  NOTES_RATE_REQUEST,
+  NOTES_RATE_SUCCESS,
+} from "../constants/noteConstants";
+import axios from "axios";
+import uri from "../uri";
 
-  
+export const rateNote = (noteId, rating) => async (dispatch, getState) => {
+  try {
+    dispatch({
+      type: NOTES_RATE_REQUEST,
+    });
+
+    const {
+      userLogin: { userInfo },
+    } = getState();
+
+    const config = {
+      headers: {
+        Authorization: `Bearer ${userInfo.token}`,
+      },
+    };
+
+    const { data } = await axios.put(
+      uri + `/api/rating/${noteId}/rating`,
+      {
+        userId: userInfo._id,
+        rating,
+      },
+      config
+    );
+
+    dispatch({
+      type: NOTES_RATE_SUCCESS,
+      payload: data,
+    });
+  } catch (error) {
+    const message =
+      error.response && error.response.data.message
+        ? error.response.data.message
+        : error.message;
+    dispatch({
+      type: NOTES_RATE_FAIL,
+      payload: message,
+    });
+  }
+};
